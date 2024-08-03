22e2d626e9b01c6b50b481572878db0a615e6464
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2024-08-03 16:11:27 +0300

    1460

--- 1. Easy/(8) Aug 2024/Week 1/2. 1460. Make Two Arrays Equal by Reversing Subarrays.py
+++ 1. Easy/(8) Aug 2024/Week 1/2. 1460. Make Two Arrays Equal by Reversing Subarrays.py
@@ -1,2 +1,8 @@
-def canBeEqual(target: list[int], arr: list[int]) -> bool:
+from collections import Counter
+def canBeEqual_nlogn(target: list[int], arr: list[int]) -> bool:
     return sorted(target) == sorted(arr)
+
+def canBeEqual_n(target: list[int], arr: list[int]) -> bool:
+    freq_1 = Counter(target)
+    freq_2 = Counter(arr)
+    return freq_1 == freq_2
