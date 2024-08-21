6cb707f4cce325d1c3586f6f86d2e9432d21fa1f
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2024-08-22 00:55:07 +0300

    145

---
+++ 1. Easy/(8) Aug 2024/Week 3/3. 145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> list[int]:
+        traversal_values = []
+
+        def post_order(root):
+            if root == None:
+                return
+
+            post_order(root.left)
+            post_order(root.right)
+            traversal_values.append(root.val)
+
+        return traversal_values
