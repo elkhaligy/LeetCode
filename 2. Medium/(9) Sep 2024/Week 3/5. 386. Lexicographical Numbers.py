2f71e20db91aa419e8c751df847ed18db9cf6fd4
Elkhaligy Laptop <shehabelkhaligy100@gmail.com>
2024-09-22 19:15:45 +0300

    386 O(n)

--- 2. Medium/(9) Sep 2024/Week 3/5. 386. Lexicographical Numbers.py
+++ 2. Medium/(9) Sep 2024/Week 3/5. 386. Lexicographical Numbers.py
@@ -2,5 +2,21 @@ def lexicalOrder_nlogn(n: int) -> list[int]:
     lst = [str(num + 1) for num in range(n)]
     return list(map(int, sorted(lst)))
 
+def lexicalOrder_n(n: int) -> list[int]:
+    result = []
+    def generate_lexical(current_number, limit) -> None:
+        if current_number > limit:
+            return
+        result.append(current_number)
+        for i in range(10):
+            next_num = current_number * 10 + i
+            if next_num <= limit:
+                generate_lexical(next_num, limit)
+            else:
+                break
 
-print(lexicalOrder_nlogn(13))
+    for i in range(1, 10):
+        generate_lexical(i, n)
+    return result
+
+print(lexicalOrder_n(13))
